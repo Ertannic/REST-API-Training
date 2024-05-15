330cc760bca83bfb54e052ef14783dcfd3838487
Erzhan <saralaev03@mail.ru>
2024-05-15 20:49:46 +0600

    Initial Commit

---
+++ REST API Training/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  REST API Training
+//
+//  Created by Ertannic Saralay on 15.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
